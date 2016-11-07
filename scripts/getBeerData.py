94a91affb1558aea84b8db305914598d4bf94271
mooshu1x2 <mbeard@draper.com>
2016-11-07 13:17:58 -0500

    Dockerfiles to ingest and parse data using ELK stack.

--- scripts/getBeerData.py
+++ scripts/getBeerData.py
@@ -1,41 +1,43 @@
 import requests
 import json
 
-# base_url = 'https://api.brewerydb.com/v2/search'
-base_url = 'https://api.brewerydb.com/v2/beers'
+payload = {'search': {'withBreweries': 'Y',
+                      'withSocialAccounts': 'N',
+                      'withGuilds': 'N',
+                      'withLocations': 'Y',
+                      'withAlternateNames': 'N',
+                      'withIngredients': 'Y',
+                      'key': 'b43137df0f9192a839a5852cadc09afc',
+                      'p': 1,
+                      'q': 'a',
+                      'format': 'json'},
+           'beers': {'withBreweries': 'N',
+                     'withSocialAccounts': 'N',
+                     'withIngredients': 'Y',
+                     'key': 'b43137df0f9192a839a5852cadc09afc',
+                     'p': 1,
+                     'format': 'json'}}
 
-# payload = {'withBreweries': 'Y',
-#            'withSocialAccounts': 'N',
-#            'withGuilds': 'N',
-#            'withLocations': 'Y',
-#            'withAlternateNames': 'N',
-#            'withIngredients': 'Y',
-#            'key': 'b43137df0f9192a839a5852cadc09afc',
-#            'p': 1,
-#            'q': 'a',
-#            'format': 'json'}
 
-payload = {'withBreweries': 'Y',
-           'withSocialAccounts': 'N',
-           'withIngredients': 'Y',
-           'key': 'b43137df0f9192a839a5852cadc09afc',
-           'p': 1,
-           'format': 'json'}
+def getBeer(base_url="", payload=dict, filename=""):
+    # All the data for the first page
+    r = requests.get(base_url, params=payload)
+    res = json.loads(r.text)
+    total = res['numberOfPages']
 
-r = requests.get(base_url, params=payload)
+    with open(filename, 'w') as outfile:
+        for x in range(1, total+1):
+            payload['p'] = x
+            r = requests.get(base_url, params=payload)
+            res = json.loads(r.text)
+            print(res['currentPage'])
+            json.dump(res['data'], outfile)
+            outfile.write('\n')
 
-# All the data for the first page
-res = json.loads(r.text)
 
-total = res['numberOfPages']
+if __name__ == '__main__':
+    beers_url = 'https://api.brewerydb.com/v2/beers'
+    search_url = 'https://api.brewerydb.com/v2/search'
+    styles_url = 'https://api.brewerydb.com/v2/styles'
 
-# for i in range(ord('a'), ord('z') + 1):
-#     payload['q'] = chr(i)
-
-with open('data.txt', 'w') as outfile:
-    for x in range(1, total):
-        payload['p'] = x
-        r = requests.get(base_url, params=payload)
-        res = json.loads(r.text)
-        print(res['currentPage'])
-        json.dump(res, outfile)
+    getBeer(beers_url, payload["beers"], "data/beers.json")

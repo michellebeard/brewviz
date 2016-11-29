92eba414c3f0d4be3a4b6c128a5fc45682e8d805
mooshu1x2 <mbeard@draper.com>
2016-11-28 19:53:47 -0500

    Final update on data processing scripts.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,5 @@
 echo "Deleting index"
-curl -XDELETE http://localhost:9200/beer?pretty --user "elastic:changeme"
-curl -XDELETE http://localhost:9200/brew?pretty --user "elastic:changeme"
+curl -XDELETE http://localhost:9200/brew?pretty 
 
 echo "Deleting template"
-curl -XDELETE http://localhost:9200/_template/beer?pretty --user "elastic:changeme"
-curl -XDELETE http://localhost:9200/_template/brew?pretty --user "elastic:changeme"
+curl -XDELETE http://localhost:9200/_template/brew?pretty
--- scripts/getBeerData.py
+++ scripts/getBeerData.py
@@ -19,14 +19,17 @@ payload = {'search': {'withBreweries': 'Y',
                      'format': 'json'}}
 
 
-def getBeer(base_url="", payload=dict, filename=""):
+def getBeer(base_url="", payload=dict, prefix=""):
     # All the data for the first page
     r = requests.get(base_url, params=payload)
     res = json.loads(r.text)
     total = res['numberOfPages']
+    ext = '.json'
 
-    with open(filename, 'w') as outfile:
-        for x in range(1, total+1):
+    for x in range(1, total + 1):
+        fmt = "%04d" % (x)
+        filename = prefix + "-" + fmt + ext
+        with open(filename, 'w') as outfile:
             payload['p'] = x
             r = requests.get(base_url, params=payload)
             res = json.loads(r.text)
@@ -40,4 +43,4 @@ if __name__ == '__main__':
     search_url = 'https://api.brewerydb.com/v2/search'
     styles_url = 'https://api.brewerydb.com/v2/styles'
 
-    getBeer(beers_url, payload["beers"], "data/beers.json")
+    getBeer(beers_url, payload["beers"], "data/beers")
